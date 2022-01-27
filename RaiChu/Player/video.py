a2482a722b0ca400755b42a6e07ba873d4ef271d
eVEXERA <96389440+eVEXERA@users.noreply.github.com>
2022-01-27 12:10:55 +0530

    Update video.py

--- RaiChu/Player/video.py
+++ RaiChu/Player/video.py
@@ -182,8 +182,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/STARZ_BOTS"),
-                            InlineKeyboardButton("💭 Support", url=f"https://t.me/Starz_Support")
+                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/VEXERA_UPDATES"),
+                            InlineKeyboardButton("💭 Support", url=f"https://t.me/SNEHABHI_SERVER")
                         ],
                         [
                             InlineKeyboardButton("🗑 Close", callback_data="cls")
@@ -259,8 +259,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/STARZ_BOTS"),
-                            InlineKeyboardButton("💭 Support", url=f"https://t.me/Starz_Support")
+                            InlineKeyboardButton("📣 Channel", url=f"https://t.me/VEXERA_UPDATES"),
+                            InlineKeyboardButton("💭 Support", url=f"https://t.me/SNEHABHI_SERVER")
                         ],
                         [
                             InlineKeyboardButton("🗑 Close", callback_data="cls")
